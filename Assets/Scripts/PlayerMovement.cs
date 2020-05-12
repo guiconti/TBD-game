b0c3c8b96cfb79efc0405abe467988d31ff8bb9b
GuiMac <guibasconti@gmail.com>
2020-05-11 23:37:51 -0300

    Project setup and character controller

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterController2D))]
+public class PlayerMovement : MonoBehaviour {
+  private CharacterController2D _controller;
+  private float _inputX;
+  private bool _jump;
+
+  public int movementSpeed = 50;
+
+  void Start () {
+    _controller = this.GetComponent<CharacterController2D>();
+  }
+
+  void Update () {
+    ReadInput();
+  }
+
+  void FixedUpdate() {
+    _controller.Move(_inputX * Time.fixedDeltaTime, false, _jump);
+    _jump = false;
+  }
+
+  private void ReadInput() {
+    _inputX = Input.GetAxisRaw("Horizontal") * movementSpeed;
+    if (Input.GetButtonDown("Jump")) {
+      _jump = true;
+    }
+  }
+}
