b827a6c321c248612fe7a4da62978c5de6dd5b3f
GuiMac <guibasconti@gmail.com>
2020-05-16 13:58:18 -0300

    Add Slash effect

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Attack : MonoBehaviour {
+  public class GameObjectEvent : UnityEvent<GameObject> { }
+  public GameObjectEvent HitEvent;
+
+  private void Awake() {
+    if (HitEvent == null) {
+      HitEvent = new GameObjectEvent ();
+    }
+  }
+
+  private void OnTriggerEnter(Collider other) {
+    if (other.CompareTag("Enemy")) {
+      HitEvent.Invoke(other.gameObject);
+    }
+  }
+
+  public void Destroy() {
+    GameObject.Destroy(this.gameObject);
+  }
+}
